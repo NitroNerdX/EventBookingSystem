d12daf9ecfbe9178bb2dd5eaee1881887fd47522
NitroNerdX <harshvardhanrao555@gmail.com>
2026-08-29 19:27:59 +0530

    AuthService added

---
+++ src/main/java/com/event/booking/service/AuthService.java
@@ -0,0 +1,16 @@
+package com.event.booking.service;
+
+import com.event.booking.dto.request.LoginRequest;
+import com.event.booking.dto.request.RegisterRequest;
+import com.event.booking.dto.response.AuthResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    public AuthResponse register(RegisterRequest request) {
+        throw new UnsupportedOperationException("TODO: Step 10");
+    }
+    public AuthResponse login(LoginRequest request) {
+        throw new UnsupportedOperationException("TODO: Step 10");
+    }
+}
